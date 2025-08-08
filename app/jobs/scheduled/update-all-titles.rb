a86f89a3ab0d70d89fd4be5f998bcbaf0886c782
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-08 11:04:52 +0900

    Update update-all-titles.rb

--- app/jobs/scheduled/update-all-titles.rb
+++ app/jobs/scheduled/update-all-titles.rb
@@ -1,35 +1,39 @@
 # frozen_string_literal: true
 
-module AddTitleBasedOnTrustLevel
-  class UpdateTitles < ::Jobs::Scheduled
-    every SiteSetting.update_title_frequency.hours
+module ::Jobs
+  class UpdateTitlesByGroupTL < ::Jobs::Scheduled
+    every 6.hours   # 定期执行，也可按需调整间隔
 
     def execute(args)
-      group_titles = JSON.parse(SiteSetting.group_trust_level_titles || "{}")
+      mapping_json = SiteSetting.group_tl_to_title_map
+      group_map = JSON.parse(mapping_json) rescue {}
 
-      User.find_each do |user|
-        # 跳过管理员和版主
-        next if user.admin? || user.moderator?
-
-        group_id = user.primary_group_id
-        next unless group_id
-
-        group = Group.find_by(id: group_id)
-        next unless group
-
-        group_key = group.name.downcase
-        tl = user.trust_level
-
-        titles = group_titles[group_key]
-        next unless titles.is_a?(Array)
-        next unless tl >= 1 && tl <= 4
-
-        new_title = titles[tl]
-        next unless new_title.present?
-
-        # 只在title不一致时更新
-        user.update_column(:title, new_title) if user.title != new_title
+      # 生成条件和title CASE语句
+      sql_cases = []
+      group_map.each do |group_name, tl_titles|
+        next unless tl_titles.is_a?(Array)
+        tl_titles.each_with_index do |title, tl|
+          next if title.to_s.strip == ""
+          sql_cases << <<~SQL
+            WHEN users.primary_group_id = (SELECT id FROM groups WHERE name = '#{group_name}') AND users.trust_level = #{tl}
+            THEN '#{title.gsub("'", "''")}'
+          SQL
+        end
       end
+
+      return if sql_cases.empty?
+
+      sql = <<~SQL
+        UPDATE users
+        SET title = CASE
+          #{sql_cases.join("\n")}
+          ELSE title
+        END
+        WHERE users.primary_group_id IS NOT NULL;
+      SQL
+
+      # 执行SQL
+      DB.exec sql
     end
   end
 end
