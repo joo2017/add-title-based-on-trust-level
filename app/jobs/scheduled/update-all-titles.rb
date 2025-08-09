81851c9152ffdbbd668adacd610c5e90fe96034e
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-09 11:33:53 +0900

    Update update-all-titles.rb

--- app/jobs/scheduled/update-all-titles.rb
+++ app/jobs/scheduled/update-all-titles.rb
@@ -1,34 +1,31 @@
-# frozen_string_literal: true
-
 module ::AddTitleByGroupTrust
-  class UpdateTitlesByGroupTrust < ::Jobs::Scheduled
-    every SiteSetting.add_title_by_group_trust_frequency.hours
+  class UpdateTitles < ::Jobs::Scheduled
+    every SiteSetting.update_title_frequency.hours
 
     def execute(args)
-      titles_config = SiteSetting.group_trust_level_titles
-      return unless titles_config.present?
-      begin
-        rules = JSON.parse(titles_config)
-      rescue JSON::ParserError => e
-        Rails.logger.error("add-title plugin: JSON parse error in group_trust_level_titles: #{e.message}")
-        return
-      end
+      return unless SiteSetting.add_title_by_group_trust_enabled
 
-      User.where.not(primary_group_id: nil).find_each do |user|
-        next if user.admin? || user.moderator?
+      mappings = JSON.parse(SiteSetting.group_trust_level_titles || '{}')
 
-        group = Group.find_by(id: user.primary_group_id)
-        next unless group
-        group_key = group.name.downcase
-        tl = user.trust_level.to_i
+      # 遍历每组规则，构造 SQL CASE WHEN 批量赋值
+      User.transaction do
+        mappings.each do |group_key, titles|
+          next unless titles.is_a?(Array)
 
-        tl_titles = rules[group_key]
-        next unless tl_titles.is_a?(Array)
-        next unless tl > 0 && tl < tl_titles.length
-        new_title = tl_titles[tl]
-        next if !new_title.present? || user.title == new_title
+          (1...titles.size).each do |tl|
+            title = titles[tl]
+            next if title.blank?
 
-        user.update_column(:title, new_title)
+            # 找到对应 primary_group 且 trust_level 匹配 的用户
+            group = Group.find_by("LOWER(name) = ?", group_key.downcase)
+            next unless group
+
+            # 只批量给普通用户赋值
+            User.where(primary_group_id: group.id, trust_level: tl, admin: false, moderator: false)
+                .where.not(title: title)
+                .update_all(title: title)
+          end
+        end
       end
     end
   end
