da291b27536646579ab781d280c119c3790d9b5e
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-08-09 22:30:54 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,10 @@
-# ... (plugin header)
+# frozen_string_literal: true
+
+# name: add-title-by-group-trust-level
+# about: Assign titles based on primary group and trust level.
+# version: 2.1.0
+# authors: Your Name
+
 enabled_site_setting :add_title_by_group_trust_enabled
 
 after_initialize do
